cb2085dbbca9d7e140894a76d46c2b3d9ede28b9
carloscasciano <carlos.casciano@gmail.com>
2020-06-03 17:56:45 -0300

    Fix on http to https on fetch

--- src/fetchData/fetchQuestions.js
+++ src/fetchData/fetchQuestions.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 async function axiosGetRawQuestionsData(language) {
   try {
     const response = await axios.get(
-      `http://young-wildwood-47504.herokuapp.com/${language}/questions.json`
+      `https://young-wildwood-47504.herokuapp.com/${language}/questions.json`
     )
     return response.data
   } catch (error) {
